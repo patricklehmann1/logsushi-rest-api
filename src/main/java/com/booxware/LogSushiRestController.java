71f59cb3c275af5f9f6b3b38324bc9669e53d352
Patrick Lehmann <patrick.lehmann@booxdev.de>
2015-11-12 20:10:59 +0100

    spring version version++

--- src/main/java/com/booxware/LogSushiRestController.java
+++ src/main/java/com/booxware/LogSushiRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class LogSushiRestController {
 	
